886e314846cdf4960239924b3739fc7a8a3181ec
shogo1007 <67449307+wad-sho@users.noreply.github.com>
2022-12-11 16:16:46 +0900

    Create sales_status.rb

---
+++ app/models/sales_status.rb
@@ -0,0 +1,15 @@
+class SalesStatus < ActiveHash::Base
+  self.data = [
+    { id: 0, name: '---' },
+    { id: 1, name: '新品未使用' },
+    { id: 2, name: 'ほぼ未使用' },
+    { id: 3, name: '目立った損傷なし' },
+    { id: 4, name: 'ちょっと汚いねえ' },
+    { id: 5, name: 'きちゃねえ' },
+    { id: 6, name: 'えっまじ?こんなん買うん?レベル' }
+  ]
+  include ActiveHash::Associations
+  has_many :items
+
+  
+end
